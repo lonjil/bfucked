a64a2945a3f19e230853cdad3f7f833255635d9f
Birk Hirdman <lonjil@gmail.com>
2018-01-01 16:24:08 +0100

    Add inline input with '!'.

--- bfucked.lisp
+++ bfucked.lisp
@@ -20,26 +20,36 @@
                     `((go ,s)
                       ,c))))
 (defvar *debug* '('((loop for i from (- pos 3) to (+ pos 3) do (format t "~a~%" (aref data i))))))
+(defvar *input* '((setf inline-input (1+ i)) (return prog)))
 
 (defmacro mkcompiler ()
-  `(defun bfcompile (str &aux
-                           (stack (list)))
-    `(let ((pos 30000)
-           (data (make-array 60000
-                             :element-type '(unsigned-byte 8)
-                             :initial-element 0)))
-       (tagbody
-          ,@(loop :for char :across str
-                  :appending (switch (char)
-                               (#\+ ,@*plus*)
-                               (#\- ,@*minus*)
-                               (#\> ,@*right*)
-                               (#\< ,@*left*)
-                               (#\. ,@*stop*)
-                               (#\, ,@*comma*)
-                               (#\[ ,@*open*)
-                               (#\] ,@*close*)
-                               (#\% ,@*debug*)))))))
+  `(defun bfcompile (str)
+     (let* ((inline-input nil)
+            (stack (list))
+            (compiled `(let ((pos 30000)
+                             (data (make-array 60000
+                                               :element-type '(unsigned-byte 8)
+                                               :initial-element 0)))
+                         (tagbody
+                            ,@(loop :for char :across str
+                                    :for i :from 0
+                                    :append (switch (char)
+                                              (#\+ ,@*plus*)
+                                              (#\- ,@*minus*)
+                                              (#\> ,@*right*)
+                                              (#\< ,@*left*)
+                                              (#\. ,@*stop*)
+                                              (#\, ,@*comma*)
+                                              (#\[ ,@*open*)
+                                              (#\] ,@*close*)
+                                              (#\! ,@*input*)
+                                              (#\% ,@*debug*))
+                                      :into prog
+                                    :finally (return prog))))))
+       (if inline-input
+           `(with-input-from-string (*standard-input* ,(subseq str inline-input))
+              ,compiled)
+           compiled))))
 (mkcompiler)
 (defmacro brainfuck (str)
   (bfcompile str))
